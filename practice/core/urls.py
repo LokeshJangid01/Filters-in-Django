4a22d39bde0989f3232d15c5a93dd0b7915779d3
Lokesh Jangid <=>
2024-04-08 23:23:37 +0530

    cbv added

--- practice/core/urls.py
+++ practice/core/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import home
+from .views import home, BookListView
 
 urlpatterns = [
     
-    path('',home, name='index'),
+    path('',BookListView.as_view(), name='index'),
 ]
--- practice/core/views.py
+++ practice/core/views.py
@@ -1,7 +1,10 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render
 from .forms import BookNameFilterForm
 from .models import Book
 from .filters import BookFilter
+from django.views.generic.list import ListView
 # Create your views here.
 def home(request):
     # name = request.GET.get('name')
@@ -20,3 +23,18 @@ def home(request):
         'books':book_filter.qs
     }
     return render(request,'index.html',context=context)
+
+class BookListView(ListView):
+    queryset = Book.objects.all()
+    template_name = 'index.html'
+    context_object_name = 'books'
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        self.filterset = BookFilter(self.request.GET, queryset=queryset)
+        return self.filterset.qs
+    
+    def get_context_data(self, **kwargs: Any):
+        context = super().get_context_data(**kwargs)
+        context['form'] = self.filterset.form
+        return context
